7c301f962fc725e8d836498589c82ff6eb1f535e
Rohit Nair P <rohitnairmuttathethu@gmail.com>
2026-05-20 13:28:35 +0530

    feat: add express server health check endpoint and verified db connection.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'LeadFlow API is running' });
+});
 
 const PORT = process.env.PORT || 5000;
 
